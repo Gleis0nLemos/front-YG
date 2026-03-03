09a6e13a826b47b179bc995574beb815b5d3e8fa
Gleison Lemos <gleison04lemos@gmail.com>
2026-03-02 21:53:25 -0300

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',       
-      },
-      {
-        protocol: 'http',
-        hostname: '**',       
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',         
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: '**',       
+    //   },
+    //   {
+    //     protocol: 'http',
+    //     hostname: '**',       
+    //   },
+    //   {
+    //     protocol: 'http',
+    //     hostname: 'localhost',
+    //     port: '3000',         
+    //   },
+    // ],
     unoptimized: true,
   },
 
